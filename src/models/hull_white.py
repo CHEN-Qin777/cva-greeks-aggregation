ba88c5fcbae8905a239647181d5f78d2c5ec93a3
CHEN-Qin777 <francaisedelaiwen@gmail.com>
2026-02-28 18:44:33 +0100

    Create hull_white.py

---
+++ src/models/hull_white.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Modèle Hull-White pour les taux (simplifié)."""
+import numpy as np
+
+def simuler_hull_white(r0, theta, a, sigma, T, n_steps, n_paths):
+    # À implémenter
+    pass
